84350b7fe93ffb83729da1d61cc60f69acc57e07
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 00:02:07 -0300

    adicionando campos serializer do student

--- api/serializers/student_serializer.py
+++ api/serializers/student_serializer.py
@@ -4,4 +4,4 @@ from core.models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ('name', 'email', 'cpf', 'RA')
+        fields = ('name', 'email', 'cpf', 'RA', 'birthday', 'rg', 'active', 'password')
