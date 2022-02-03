5472779f7b4047e11b73e2fa5dc64e3905edd7a8
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-02 23:22:00 -0300

    adicinando atributos user

--- core/models/user.py
+++ core/models/user.py
@@ -1,4 +1,8 @@
+from turtle import update
+from venv import create
 from django.db import models
+from django import forms
+
 
 class User(models.Model):
     """Abstract class, represents a user."""
@@ -6,6 +10,12 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     cpf = models.CharField(max_length=11)
+    rg = models.CharField(max_length=30)
+    birthday = models.DateField()
+    password = models.CharField(max_length=20)
+    active = models.BooleanField(default=False)
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
 
     class Meta:
         abstract = True
