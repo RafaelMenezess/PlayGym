9fa0dac9aab829ac0c00c95f3b293fd784c24cc8
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 21:13:29 -0300

    retirando import não necessario

--- core/models/student.py
+++ core/models/student.py
@@ -1,9 +1,9 @@
-from dataclasses import field
 from core.models import User
 from django.db import models
 
+
 class Student(User):
+    RA = models.CharField(max_length=10)        
+    
     class Meta:
         db_table = "student"
-    
-    RA = models.CharField(max_length=10)
