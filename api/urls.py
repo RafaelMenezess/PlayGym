2825771ee95652e06ea8e544f0c1ecf7bc71876d
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 22:18:59 -0300

    adicionandot routers

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
-from api.views import StudentViewSet
+from api.viewers import StudentViewSet, AdminViewSet, DoctorViewSet, InstructorViewSet 
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 router.register(r'student', StudentViewSet)
+router.register(r'admin', AdminViewSet)
+router.register(r'doctor', DoctorViewSet)
+router.register(r'instructor', InstructorViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
