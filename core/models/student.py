c0faa9bfee1e9d7e665cad6e8fc3d6756444a65d
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 22:21:40 -0300

    alterando RA

--- core/models/student.py
+++ core/models/student.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Student(User):
-    RA = models.CharField(max_length=10)        
+    registration_number = models.CharField(max_length=10)        
     
     class Meta:
         db_table = "student"
