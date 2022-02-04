8e22d3c2676e185c3a682a6f6dad2c62d2d04813
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 22:17:35 -0300

    criando doctor_viewer

---
+++ api/viewers/doctor_viewer.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+from core.models import Doctor
+from api.serializers import DoctorSerializer
+from rest_framework import viewsets
+
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    '''List all User, or create a new User.'''
+
+    queryset = Doctor.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = DoctorSerializer
