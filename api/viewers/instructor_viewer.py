9783dffef28c50879e6b477cc1e126ababe0a7da
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 22:17:51 -0300

    criando instructor_viewer

---
+++ api/viewers/instructor_viewer.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+from core.models import Instructor
+from api.serializers import InstructorSerializer
+from rest_framework import viewsets
+
+
+
+class InstructorViewSet(viewsets.ModelViewSet):
+    '''List all User, or create a new User.'''
+
+    queryset = Instructor.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = InstructorSerializer
