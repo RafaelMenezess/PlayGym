f3ed135e194847eca4e8a7e3be2c395b45b5a9ff
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 22:21:21 -0300

    adicionando imports

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,2 +1,5 @@
 from core.models.user import User
 from core.models.student import Student
+from core.models.doctor import Doctor
+from core.models.admin import Admin
+from core.models.instructor import Instructor
