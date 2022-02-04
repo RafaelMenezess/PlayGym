a7029fae7198a4923b019d5c72ce32fe850fc222
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-03 22:15:40 -0300

    criando student_serializer

--- api/serializers/student_serializer.py
+++ api/serializers/student_serializer.py
@@ -4,4 +4,4 @@ from core.models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ('name', 'email', 'cpf', 'RA', 'birthday', 'rg', 'active', 'password')
+        fields = ('name', 'email', 'cpf', 'registration_number', 'birthday', 'rg', 'active', 'password')
