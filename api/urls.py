5ad6e505e92d9d7d0cf23a93e0b0f7ccb7f233a3
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-16 22:15:04 -0300

    adicionado tolken jwt

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path, include
-from api.viewers import StudentViewSet, AdminViewSet, DoctorViewSet, InstructorViewSet 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from api.viewers import (
+    StudentViewSet,
+    AdminViewSet,
+    DoctorViewSet,
+    InstructorViewSet,
+)
 
 
 router = DefaultRouter()
-router.register(r'student', StudentViewSet)
-router.register(r'admin', AdminViewSet)
-router.register(r'doctor', DoctorViewSet)
-router.register(r'instructor', InstructorViewSet)
-
+router.register(r"student", StudentViewSet)
+router.register(r"admin", AdminViewSet)
+router.register(r"doctor", DoctorViewSet)
+router.register(r"instructor", InstructorViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("api/", include(router.urls)),
+    path("token", TokenObtainPairView.as_view(), name="token_obtain_pai"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
