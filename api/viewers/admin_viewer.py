ba4e07f47e64b9df30d82af43f1d0bf36a7d9f58
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-28 18:22:23 -0300

    Alterando imports para viewsets

--- api/viewers/admin_viewer.py
+++ api/viewers/admin_viewer.py
@@ -1,11 +1,11 @@
 from rest_framework import permissions
-from rest_framework import viewsets
+from api.viewers.custom_model_view_set import CustomModelViewSet
 
 from api.serializers import AdminSerializer
 from core.models import Admin
 
 
-class AdminViewSet(viewsets.ModelViewSet):
+class AdminViewSet(CustomModelViewSet):
     """List all User, or create a new User."""
 
     queryset = Admin.objects.all()
