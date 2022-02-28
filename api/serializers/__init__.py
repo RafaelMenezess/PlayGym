05918ddbb6eb817f410a01de12ccae17dc11402e
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-28 18:19:53 -0300

    Adicionando imports

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -2,3 +2,6 @@ from api.serializers.student_serializer import StudentSerializer
 from api.serializers.admin_serializer import AdminSerializer
 from api.serializers.doctor_serializer import DoctorSerializer
 from api.serializers.instructor_serializer import InstructorSerializer
+from api.serializers.student_schedule_serializer import StudentScheduleSerializer
+from api.serializers.schedule_config_serializer import ScheduleConfigSerializer
+from api.serializers.doctor_schedule_serializer import DoctorScheduleSerializer
