cfbf095100f0cf0e87b150120daa989b9924b889
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-28 18:21:42 -0300

    Adicionando imports

--- api/viewers/__init__.py
+++ api/viewers/__init__.py
@@ -2,3 +2,6 @@ from api.viewers.admin_viewer import AdminViewSet
 from api.viewers.doctor_viewer import DoctorViewSet
 from api.viewers.instructor_viewer import InstructorViewSet
 from api.viewers.student_viewer import StudentViewSet
+from api.viewers.schedule_config_viewer import ScheduleConfigView
+from api.viewers.student_schedule_viewer import StudentScheduleView, StudentScheduleDetailsView
+from api.viewers.doctor_schedule_viewer import DoctorScheduleView, DoctorScheduleDetailsView
