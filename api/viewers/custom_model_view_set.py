4637de31ad9877e269cd36a822b0806c43f5392d
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-28 18:22:39 -0300

    Criando CustomModelViewSet

---
+++ api/viewers/custom_model_view_set.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, mixins
+
+
+class CustomModelViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    pass
