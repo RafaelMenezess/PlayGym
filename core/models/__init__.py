d610452bd13ddf775852a603c49e54aae1515c00
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-28 18:27:15 -0300

    Adicionando imports

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -3,3 +3,6 @@ from core.models.student import Student
 from core.models.doctor import Doctor
 from core.models.admin import Admin
 from core.models.instructor import Instructor
+from core.models.student_schedule import StudentSchedule
+from core.models.schedule_config import ScheduleConfig
+from core.models.doctor_schedule import DoctorSchedule
