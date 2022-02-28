c53e73a33ec19d7c35774f1cfdf51bae3072fc53
Rafael Menezes <rafa.rcm@hotmail.com>
2022-02-28 18:24:33 -0300

    Alterando import para CustomModelViewSet

--- api/viewers/instructor_viewer.py
+++ api/viewers/instructor_viewer.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions
 from core.models import Instructor
 from api.serializers import InstructorSerializer
-from rest_framework import viewsets
+from api.viewers.custom_model_view_set import CustomModelViewSet
 
 
-class InstructorViewSet(viewsets.ModelViewSet):
+class InstructorViewSet(CustomModelViewSet):
     """List all User, or create a new User."""
 
     queryset = Instructor.objects.all()
