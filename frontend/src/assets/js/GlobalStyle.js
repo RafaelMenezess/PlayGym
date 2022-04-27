1231376202b2e613f2272ed1e49db112a870da9e
Rafael Menezes <rafa.rcm@hotmail.com>
2022-04-26 21:46:26 -0300

    Adiciona style global

---
+++ frontend/src/assets/js/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html, body {
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 100%;
+    }
+
+    #root{
+        height: 100%;
+    }
+`;
