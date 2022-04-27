a2da4082d0e2349d1a181266f0fa1a8e16e17039
Rafael Menezes <rafa.rcm@hotmail.com>
2022-04-26 21:56:16 -0300

    Adicionando cofiguração cors e csrf

--- playgym/settings.py
+++ playgym/settings.py
@@ -162,7 +162,17 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = ("http://localhost:3000",)
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
 
 AUTH_USER_MODEL = "core.CustomUser"
 
