6fbde4b61b93f40861e6ea4ba7e6ff6a0a9b160a
Rafael Menezes <rafa.rcm@hotmail.com>
2022-06-01 19:55:43 -0300

    adicionando carrosel main

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,24 +1,52 @@
 import React from 'react'
-import Calendar from '../../components/Calendar/Calendar';
+import { Carousel } from 'react-bootstrap';
 
 function Main() {
     return (
-        <div className='calendar-container'>
-            <div className="container-fluid text-center">
-                <div class="row content">
-                    <div class="col-sm-10 text-left">
-                        <Calendar />
-                    </div>
-                    <div class="col-sm-2 sidenav">
-                        <div class="well">
-                            <h3>NOTICIAS</h3>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printening Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                        </div>
-                        <div class="well">
-                            <h3>AVISOS</h3>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarr.</p>
-                        </div>
-                    </div>
+        <div>
+            <Carousel variant="dark">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=First slide&bg=f5f5f5"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Second slide&bg=eee"
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h5>Third slide label</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <div className="col-sm-2 sidenav">
+                <div className="well">
+                    <h3>NOTICIAS</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printening Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                <div className="well">
+                    <h3>AVISOS</h3>
+                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarr.</p>
                 </div>
             </div>
         </div>
