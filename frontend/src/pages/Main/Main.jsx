b52e91b36ff6856e75b7177a8e206cc1b6f1ca47
Rafael Menezes <rafa.rcm@hotmail.com>
2022-06-28 21:18:25 -0300

    Adicionando CSS na page Main

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -1,52 +1,64 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap';
+import { BsChatLeftTextFill, BsExclamationTriangleFill, BsFillCalendar2RangeFill } from 'react-icons/bs'
+import "./Main.css";
 
 function Main() {
     return (
-        <div>
+        <div className='carousel-div'>
             <Carousel variant="dark">
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=First slide&bg=f5f5f5"
+                        src={require("../../../src/assets/images/banner.jpg")}
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="holder.js/800x400?text=Second slide&bg=eee"
+                        src={require("../../../src/assets/images/banner2.jpg")}
                         alt="Second slide"
                     />
                     <Carousel.Caption>
-                        <h5>Second slide label</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-                        alt="Third slide"
-                    />
-                    <Carousel.Caption>
-                        <h5>Third slide label</h5>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            <div className="col-sm-2 sidenav">
-                <div className="well">
-                    <h3>NOTICIAS</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printening Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-                <div className="well">
-                    <h3>AVISOS</h3>
-                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarr.</p>
+            <div class="b-example-divider"></div>
+            <div className='front-board'>
+                <div class="container px-4 py-5" id="hanging-icons">
+                    <h2 class="pb-2 border-bottom">Informações</h2>
+                    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                        <div class="col d-flex align-items-start">
+                            <div class="icon-square text-white d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                                <BsChatLeftTextFill />
+                            </div>
+                            <div>
+                                <h2>Noticias</h2>
+                                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                            </div>
+                        </div>
+                        <div class="col d-flex align-items-start">
+                            <div class="icon-square text-white d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                                <BsExclamationTriangleFill />
+                            </div>
+                            <div>
+                                <h2>Avisos</h2>
+                                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                            </div>
+                        </div>
+                        <div class="col d-flex align-items-start">
+                            <div class="icon-square text-white d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+                                <BsFillCalendar2RangeFill />
+                            </div>
+                            <div>
+                                <h2>Agenda</h2>
+                                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
